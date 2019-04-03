4a62f352cbf09e0a7be8862913a89bd1eb30909c
Philipp Meier <philipp.meier@meteoswiss.ch>
2019-04-03 14:55:58 +0200

    Added function to check for and optionally create folders defined in the config file.

--- yaconfigobject.py
+++ yaconfigobject.py
@@ -38,6 +38,10 @@ logger = logging.getLogger(__name__)
 CONFIGNAME = 'config.yaml'
 
 
+class ConfigError(Exception):
+    pass
+
+
 class ConfigContainer(dict):
     """A dictionary object holding all config items.
     """
@@ -46,7 +50,7 @@ class ConfigContainer(dict):
         """Load an arbitrary configuration file. Multiple files can be loaded,
         the last file loaded will take precedence over previously loaded files.
         """
-        logger.info('Loading configuration file: {}'.format(conffile))
+        logger.info('Loading configuration file: %s', conffile)
 
         with open(conffile) as f:
             self.update(yaml.load(f))
@@ -56,6 +60,10 @@ class ConfigContainer(dict):
 
     def __getattr__(self, key):
         return self.__getitem__(key)
+
+    def __add__(self, other):
+        self.update(other)
+        return self
 
     def update(self, other):
         """Modify update function for consistent handling of nested
@@ -70,6 +78,40 @@ class ConfigContainer(dict):
             else:
                 self.__setitem__(key, val)
 
+    def check_folders(self, keyword='folder', create=False):
+        """Perform check if folders exist. Folders are identified by a keyword
+        as a substring of the corresponding field name.
+
+        Kwargs:
+            keyword (str): Keyword identifying folder fields in the config
+                file. All fields containing this keyword as a substring are
+                considered.
+            create (boolean): Whether non-existing folders will be created or
+                not.
+
+        Returns:
+
+        """
+        created_folders = []
+
+        for key, val in self.items():
+            if isinstance(val, ConfigContainer):
+                val.check_folders(keyword=keyword, create=create)
+            else:
+                if keyword in key:
+                    self[key] = os.path.abspath(
+                        os.path.expanduser(os.path.expandvars(val)))
+                    logger.debug('Checking presence of %s', self[key])
+                    if not os.path.isdir(self[key]):
+                        if create:
+                            logger.info('Creating folder %s', self[key])
+                            os.makedirs(self[key])
+                            created_folders.append(self[key])
+                        else:
+                            raise ConfigError('Folder %s does not exist!',
+                                              self[key])
+        return created_folders
+
 
 class Config(ConfigContainer):
     def __init__(self, paths=None, name=None, **kwargs):
@@ -82,25 +124,26 @@ class Config(ConfigContainer):
 
             calling_frame = inspect.getframeinfo(
                 inspect.stack()[0].frame.f_back)
-            logger.debug('{}'.format(
-                inspect.getframeinfo(inspect.stack()[0].frame.f_back)))
+            logger.debug('%s',
+                         inspect.getframeinfo(inspect.stack()[0].frame.f_back))
             self._package_base = os.path.dirname(calling_frame.filename)
             calling_package = os.path.basename(self._package_base)
             paths = [
                 os.getcwd(),
                 os.path.abspath(
                     os.path.expanduser(
-                        os.path.join('~', '.config', '{}').format(calling_package))),
+                        os.path.join('~', '.config',
+                                     '{}').format(calling_package))),
                 os.path.join('{}', 'config').format(self._package_base),
             ]
 
         nconfig = 0
         for path in paths[::-1]:
             filepath = os.path.join(path, name)
-            logger.debug('Trying to load {}.'.format(filepath))
+            logger.debug('Trying to load %s.', filepath)
 
             if os.path.exists(filepath):
-                logger.info('Loading {}.'.format(filepath))
+                logger.info('Loading %s.', filepath)
                 self.load(filepath)
 
                 nconfig += 1
