a73657cd9e430c7cda00689e2259c23f75dc318f
azg <gabriela.aznar@meteoswiss.ch>
2020-04-28 15:26:28 +0200

    fix: correct package information.

--- setup.py
+++ setup.py
@@ -5,21 +5,55 @@
 
 from setuptools import setup
 
+requirements = [
+    'pyyaml',
+]
+
+setup_requirements = [
+    'setuptools_scm',
+]
+
+test_requirements = [
+    'pytest',
+]
+
+extras = {
+    'test': test_requirements,
+}
+
+packages = {}
+
+package_dir = {}
+
+package_data = {}
 
 setup(name='yaconfigobject',
       use_scm_version=True,
       author='Philipp Falke',
       author_email='philipp.falke@meteoswiss.ch',
       description='A library representing YAML config files as object.',
+      long_description=open('README.md').read() + '\n\n' +
+                     open('HISTORY.rst').read(),
+      long_description_content_type="text/markdown",
+      url='https://github.com/MeteoSwiss/yaconfigobject',
+      classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Natural Language :: English',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+      ],
+      license='BSD-3-Clause license',
+      keywords='yaconfigobject',
       entry_points={},
       py_modules=['yaconfigobject'],
       include_package_data=True,
-      package_data={'': ['README.md'],
-                    },
-      license='MIT License',
-      long_description=open('README.md').read(),
-      setup_requires=['setuptools_scm'],
-      install_requires=[
-          'pyyaml',
-      ]
+      install_requires = requirements,
+      package_dir = package_dir,
+      package_data = package_data,
+      setup_requires = setup_requirements,
+      tests_require = test_requirements,
+      extras_require = extras,
       )
